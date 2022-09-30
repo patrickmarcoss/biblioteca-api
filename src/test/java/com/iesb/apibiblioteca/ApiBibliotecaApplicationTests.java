22b4a8e03d9af2320e0d19c7a27569e63f798771
Gabriel Arraes Barbosa <gabrielarraesbarbosa@gmail.com>
2022-09-30 15:13:34 -0300

    first commit

---
+++ src/test/java/com/iesb/apibiblioteca/ApiBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iesb.apibiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBibliotecaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
